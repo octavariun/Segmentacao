a7e309151c2fd39bb4dfc08cb7cd2e559ae21394
Cassio Felipe <octavariun@gmail.com>
2017-05-29 22:11:15 -0300

    banco

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170528221137) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contatos", force: :cascade do |t|
     t.text     "nome"
     t.text     "email"
