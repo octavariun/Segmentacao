41a0361c2e6ff49c35d84b27355e5b9fcf392e04
Cassio Felipe <octavariun@gmail.com>
2017-05-29 22:43:51 -0300

    bootstrap

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,5 +1,10 @@
 class Contato < ActiveRecord::Base
 
+validates :nome, :email, presence: true, uniqueness: true 
+validates :idade, :estado, :cargo, presence: true
+
+
+
 def self.ransackable_attributes(*)
       %w( nome email idade estado cargo  ) + _ransackers.keys
     end
